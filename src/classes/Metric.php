684c3df425d2dd11e0c3918d9019c820d50cf566
Umut Isik <umut.isik@sony.com>
2019-10-24 14:13:59 +0300

    Abstract class

---
+++ src/classes/Metric.php
@@ -0,0 +1,32 @@
+<?php
+namespace WOSPM\Checker
+namespace UmutPHP\WPVulnerabilityCheck;
+
+class Metric {
+	private $code  = "WOSPM0001";
+
+	private $title = "Nothing is working";
+
+	private $message = "It seems nothing is working.";
+
+	public function check($files) {
+		
+	}
+
+	private function success() {
+		return $this->result(true);
+	}
+
+	private function fail() {
+		return $this->result();
+	}
+
+	private function result($status = false) {
+		return array(
+			"code" => $this->code,
+			"title"   => $this->title,
+			"message" => $this->message,
+			"status"  => $status
+		);
+	}
+}
