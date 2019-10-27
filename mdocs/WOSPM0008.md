39ed8dc1fba1a5851021a94e94fec5da8db36d1c
Umut Işık <umutphp@gmail.com>
2019-10-27 15:13:31 +0300

    Add ReadmeCocLinkExistsMetric

---
+++ mdocs/WOSPM0008.md
@@ -0,0 +1,17 @@
+# WOSPM0008
+
+## Idenditiy
+
+| Property        | Value           |
+| ------------- |-------------|
+| code      | WOSPM0008 |
+| title      | NO_LINK_TO_CODE_OF_CONDUCT      |
+| message | README should have a link to NO_LINK_TO_CODE_OF_CONDUCT file.     |
+| type | MetricType::ERROR      |
+
+## Rationale
+
+> A code of conduct is a document that establishes expectations for behavior for your project’s participants. Adopting, and enforcing, a code of conduct can help create a positive social atmosphere for your community.
+
+See also;
+- [Why do I need a code of conduct?](https://opensource.guide/code-of-conduct/#why-do-i-need-a-code-of-conduct)
---
+++ src/classes/ReadmeCocLinkExistsMetric.php
@@ -0,0 +1,58 @@
+<?php
+namespace WOSPM\Checker;
+
+/**
+ * Doc comment for class ReadmeCocLinkExistsMetric
+ */
+class ReadmeCocLinkExistsMetric extends Metric
+{
+    private $project;
+
+    /**
+     * Contructor function that initializes the Metric definitions
+     */
+    public function __construct()
+    {
+        $this->code       = "WOSPM0008";
+        $this->title      = "NO_LINK_TO_CODE_OF_CONDUCT";
+        $this->message    = "README should have a link to CODE_OF_CONDUCT.";
+        $this->type       = MetricType::ERROR;
+        $this->dependency = array("WOSPM0002", "WOSPM0005");
+        $this->project    = new Project();
+    }
+
+    /**
+     * Checks if there is a link to code of conduct file in readme file
+     * 
+     * @param array $files Array of the files in root directory
+     *
+     * @return array
+     */
+    public function check($files)
+    {
+        $coc = array(
+            "CODE_OF_CONDUCT", "CODE_OF_CONDUCT.md",
+            "code_of_conduct", "code_of_conduct.md"
+        );
+
+        $file   = array_values(array_intersect($coc, $files))[0];
+        $readme = $this->project->getReadme($files);
+        $count  = substr_count($readme, $file . ')');
+
+        if ($count !== 0) {
+            return $this->success();
+        }
+
+        return $this->fail();
+    }
+
+    /**
+     * Setter for the project property
+     *
+     * @param Project $project Project object
+     */
+    public function setProject($project)
+    {
+        $this->project = $project;
+    }
+}
