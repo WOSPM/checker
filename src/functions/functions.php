f5eaba8f54bc4c48727d84231eed3a51f9c7d27b
Umut Işık <umutphp@gmail.com>
2019-11-16 21:22:41 +0300

    Fix phpunit tests

--- src/functions/functions.php
+++ src/functions/functions.php
@@ -282,13 +282,15 @@ function scanAllDir($path)
         $el = $path . DIRECTORY_SEPARATOR . $element . DIRECTORY_SEPARATOR;
         if (is_dir($el)) {
             $elfiles  = scanAllDir($el);
-            $elfiles  = preg_filter('/^/', $el, $elfiles);
+            //$elfiles  = preg_filter('/^/', $el, $elfiles);
             $subfiles = array_merge($subfiles, $elfiles);
 
-            $files[$key] = $path . DIRECTORY_SEPARATOR . $element;
+            //$files[$key] = $path . DIRECTORY_SEPARATOR . $element;
+            unset($files[$key]);
             continue;
         } else {
-            $files[$key] = $el = $path . DIRECTORY_SEPARATOR . $element;
+            $files[$key] = trim($path, DIRECTORY_SEPARATOR) .
+            DIRECTORY_SEPARATOR . $element;
         }
     }
 
