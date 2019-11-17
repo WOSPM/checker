4d59c392ec48754e460c91d6919e57e499d5e1f7
Umut Işık <umutphp@gmail.com>
2019-11-17 16:55:16 +0300

    Add cli tests for cli functions

---
+++ tests/clifunctiontests/FunctionsTest.php
@@ -0,0 +1,147 @@
+<?php
+use WOSPM\Checker;
+
+require_once "." . DIRECTORY_SEPARATOR .
+    "src" . DIRECTORY_SEPARATOR . "functions" . DIRECTORY_SEPARATOR .
+    "functions.php";
+
+class FunctionsTest extends PHPUnit_Framework_TestCase
+{
+    public function testStatusEmptyTrue()
+    {
+        $array = array();
+
+        $this->assertTrue(status($array));
+    }
+
+    public function testStatusTrue()
+    {
+        $array = array(
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+        );
+
+        $this->assertTrue(status($array));
+    }
+
+    public function testStatusFalse()
+    {
+        $array = array(
+            array("status" => false),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+        );
+
+        $this->assertFalse(status($array));
+    }
+
+    public function testPercent()
+    {
+        $array = array(
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+        );
+
+        $this->assertEquals(100, percent($array));
+
+        $array = array(
+            array("status" => false),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+        );
+
+        $this->assertEquals(90, percent($array));
+
+        $array = array(
+            array("status" => false),
+            array("status" => false),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+            array("status" => true),
+        );
+
+        $this->assertEquals(80, percent($array));
+        $array = array(
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => true),
+            array("status" => true),
+        );
+
+        $this->assertEquals(20, percent($array));
+
+        $array = array(
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => true),
+        );
+
+        $this->assertEquals(10, percent($array));
+
+        $array = array(
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+            array("status" => false),
+        );
+
+        $this->assertEquals(0, percent($array));
+    }
+
+    public function testBadge()
+    {
+        $this->assertEquals("![Perfect](https://img.shields.io/badge/WOSPM-Perfect-blue)", badge(100));
+        $this->assertEquals("![Welcoming](https://img.shields.io/badge/WOSPM-Welcoming-green)", badge(95));
+        $this->assertEquals("![Welcoming](https://img.shields.io/badge/WOSPM-Welcoming-green)", badge(90));
+        $this->assertEquals("![Bad](https://img.shields.io/badge/WOSPM-Bad-red)", badge(40));
+        $this->assertEquals("![Bad](https://img.shields.io/badge/WOSPM-Bad-red)", badge(30));
+        $this->assertEquals("![Bad](https://img.shields.io/badge/WOSPM-Bad-red)", badge(0));
+        $this->assertEquals("![Not Ready](https://img.shields.io/badge/WOSPM-Not--Ready-orange)", badge(80));
+        $this->assertEquals("![Not Ready](https://img.shields.io/badge/WOSPM-Not--Ready-orange)", badge(60));
+        $this->assertEquals("![Not Ready](https://img.shields.io/badge/WOSPM-Not--Ready-orange)", badge(50));
+    }
+}
